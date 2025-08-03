beb507d82121a7af591a24482a8934fa9b4fae78
lHDHRl <asobunerupy.official@gmail.com>
2025-08-03 18:54:05 +1000

    sync

--- models/user.go
+++ models/user.go
@@ -1,11 +1,8 @@
 package models
 
 import (
-	"fmt"
-	"io"
 	"time"
 
-	"github.com/99designs/gqlgen/graphql"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -62,23 +59,3 @@ func (r UserRole) IsValid() bool {
     }
 }
 
-type ObjectID = primitive.ObjectID
-
-func MarshalObjectID(id ObjectID) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		fmt.Fprintf(w, "\"%s\"", id.Hex())
-	})
-}
-
-func UnmarshalObjectID(v interface{}) (ObjectID, error) {
-	str, ok := v.(string)
-	if !ok {
-		return primitive.NilObjectID, fmt.Errorf("ObjectID must be a string")
-	}
-	return primitive.ObjectIDFromHex(str)
-}
-func MarshalObjectIDScalar(id primitive.ObjectID) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		fmt.Fprintf(w, "\"%s\"", id.Hex())
-	})
-}

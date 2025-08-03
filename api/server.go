d72f2f09b201788973ede18c6e2774122629dd3e
lHDHRl <asobunerupy.official@gmail.com>
2025-08-03 17:35:07 +1000

    * Доимплементировал Граф, начинаю писать схему

--- api/server.go
+++ api/server.go
@@ -7,11 +7,12 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	lru "github.com/hashicorp/golang-lru"
+	"github.com/DGISsoft/DGISback/api/graph"
 	"github.com/rs/cors"
-	"github.com/vektah/gqlparser/ast"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 
